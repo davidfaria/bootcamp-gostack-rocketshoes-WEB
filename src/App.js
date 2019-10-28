cd7d2178cb9af99bbd33e691160a2c9f9d81d96d
David Faria <davidfaria89@gmail.com>
2019-10-28 18:17:30 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
+import './config/ReactotronConfig';
+import history from './services/history';
+import store from './store';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+        <Header />
+        <Routes />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/store/modules/cart/reducer.js
@@ -0,0 +1,43 @@
+import produce from 'immer';
+
+const INIT_STATE = [];
+
+export default function cart(state = INIT_STATE, action) {
+  switch (action.type) {
+    case '@cart/ADD_SUCCESS':
+      return produce(state, draft => {
+        const { product } = action;
+        draft.push(product);
+
+        // const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        // if (productIndex >= 0) {
+        //   draft[productIndex].amount += 1;
+        // } else {
+        //   draft.push({ ...action.product, amount: 1 });
+        // }
+      });
+
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
+
+    default:
+      return state;
+  }
+}
